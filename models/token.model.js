e64239d7791dc032addcaae562c3f82cb7b8aafa
MaksAlieksieiev <Maksim.Alekseev@fivesysdev.com>
2024-01-11 22:28:37 +0200

    add linter and husky

--- models/token.model.js
+++ models/token.model.js
@@ -1,23 +1,23 @@
 module.exports = (sequelize, Sequelize) => {
-    const Token = sequelize.define("token", {
-      userId: {
-        type: Sequelize.NUMBER,
-        allowNull: false
-      },
-      refreshToken: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-    });
+  const token = sequelize.define('token', {
+    userId: {
+      type: Sequelize.NUMBER,
+      allowNull: false,
+    },
+    refreshToken: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  });
 
-    Token.associate = function (models) {
-        models.userPermissionsNew.belongsTo(models.user, {
-          foreignKeyConstraint: true,
-          foreignKey: "userId",
-          onDelete: "CASCADE",
-          onUpdate: "CASCADE",
-        });
-      };
-  
-    return Token;
+  token.associate = function (models) {
+    models.token.belongsTo(models.user, {
+      foreignKeyConstraint: true,
+      foreignKey: 'userId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
   };
+
+  return token;
+};
--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors')
-const authRoutes = require('./app/routes/auth.routes.js');
-const db = require("./app/models");
+const cors = require('cors');
+const authRoutes = require('./app/routes/auth.routes');
+const db = require('./models');
 
 const port = 3000;
 const corsOptions = {
-	origin: 'http://localhost:3200'
-}
+  origin: 'http://localhost:3200',
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-db.sequelize.sync()
-.then(() => {
-	console.log("Synced db.");
-})
-.catch((err) => {
-	console.log("Failed to sync db: " + err.message);
-});
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log('Synced db.');
+  })
+  .catch((err) => {
+    console.log(`Failed to sync db: ${err.message}`);
+  });
 
 app.use('/api/v1/auth', authRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
